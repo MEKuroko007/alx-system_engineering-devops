43c1a27a0fe0c018eab64302ffc1b85a93283ce2
MEKuroko007 <essalhimu@gmail.com>
2023-12-22 15:21:52 +0100

    fix the issue of installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
 #!/usr/bin/pup
 # Using Puppet, install flask V2.1.0 from pip3.
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+class install_flask {
+  package { 'python3-pip':
+    ensure => installed,
+  }
+
+  exec { 'install_flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    path    => ['/usr/bin'],
+    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+    require => Package['python3-pip'],
+  }
 }
+
+include install_flask
