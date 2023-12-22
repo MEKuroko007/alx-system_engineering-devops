921f0e6ba96c8b102689a8141126f57f04f75426
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-12-22 22:00:16 +0100

    trying to fix the second check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,30 +1,27 @@
 #!/usr/bin/pup
-# Install an especific version of flask 2.1.0
 
+# Install a specific version of Python 3.8
 package { 'python3.8':
-  ensure   => '3.8.10',
+  ensure   => '3.8.12',  # Adjust version accordingly
   provider => 'apt',
 }
 
-exec { 'check-pip3':
-  command => '/usr/bin/which pip3',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/test -x /usr/bin/pip3',
-}
-
+# Install pip for Python 3
 package { 'python3-pip':
-  ensure  => 'installed',
+  ensure   => 'installed',
   provider => 'apt',
-  require => Exec['check-pip3'],
+  require  => Package['python3.8'],
 }
 
-
+# Install Flask and Werkzeug using pip3
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
