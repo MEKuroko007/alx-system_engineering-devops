aaec078258f2f60f7ce3adccfbb3938044bb9059
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-12-22 21:48:58 +0100

    trying to fix the second check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,30 +1,47 @@
 #!/usr/bin/pup
 # Install an especific version of flask 2.1.0
 
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'apt',
+}
 
+package { 'software-properties-common':
+  ensure   => 'installed',
+  require  => Package['python3.8'],
+}
+
+exec { 'add-apt-repository':
+  command => '/usr/bin/add-apt-repository ppa:deadsnakes/ppa',
+  path    => ['/usr/bin'],
+  user    => 'root',
+  require => Package['software-properties-common'],
+}
 
 exec { 'apt-update':
   command => '/usr/bin/apt-get update',
   path    => ['/usr/bin'],
   require => Exec['add-apt-repository'],
 }
-exec { 'install_python':
-  command => '/usr/bin/apt-get update && /usr/bin/apt-get install -y software-properties-common && /usr/bin/add-apt-repository ppa:deadsnakes/ppa && /usr/bin/apt-get update && /usr/bin/apt-get install -y python3.8',
-  path    => ['/usr/bin'],
-  user    => 'root',
-  unless  => '/usr/bin/dpkg-query --show --showformat=\'${db:Status-Status}\' python3.8 | grep -q "install"',
-}
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider => 'apt-get',
+
+package { 'python3.8-dev':
+  ensure   => 'installed',
+  require  => Exec['apt-update'],
 }
 
-package { 'Flask':
+package { 'python3.8':
+  ensure   => 'installed',
+  require  => Package['python3.8-dev'],
+}
+
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3.8'],
 }
 
-package { 'Werkzeug':
+package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
+  require  => Package['flask'],
 }
