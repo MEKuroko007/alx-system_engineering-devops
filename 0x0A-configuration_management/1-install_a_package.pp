5f1a27025c313f46683739f6538bf97f9e9e5483
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-12-22 21:52:02 +0100

    trying to fix the second check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -14,6 +14,25 @@ package { 'python3.8':
   provider => 'apt',
 }
 
+package { 'software-properties-common':
+  ensure   => 'installed',
+  require  => Package['python3.8'],
+}
+exec { 'add-apt-repository':
+  command => '/usr/bin/add-apt-repository ppa:deadsnakes/ppa',
+  path    => ['/usr/bin'],
+  user    => 'root',
+  require => Package['software-properties-common'],
+}
+package { 'python3.8-dev':
+  ensure   => 'installed',
+  require  => Exec['apt-update'],
+}
+
+package { 'python3.8':
+  ensure   => 'installed',
+  require  => Package['python3.8-dev'],
+}
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
