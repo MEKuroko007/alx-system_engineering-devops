b4f03a68fb35a733998deb0209b55e483ddd1855
MUSTAPHA ESSALHI <essalhimu@gmail.com>
2023-12-22 21:08:41 +0100

    fix problems

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,31 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# 1-install_a_package.pp
+
+package { 'python3':
+  ensure => installed,
 }
+
+package { 'python3-pip':
+  ensure => installed,
+  require => Package['python3'],
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+exec { 'install_werkzeug':
+  command => 'pip3 install Werkzeug==2.1.1',
+  unless  => 'pip3 show Werkzeug | grep Version | grep -q 2.1.1',
+  require => Package['python3-pip'],
+}
+
